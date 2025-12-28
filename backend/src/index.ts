83420c8ad443dd98e6bf60ac830102924eaa7871
vasanth <vasantkr97@gmail.com>
2025-12-28 15:06:33 +0530

    fc

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/health", (req, res) => {
+    res.send("yes healthy!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
